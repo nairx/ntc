f0f652fbb4e35e9a0505aee24e8c42ffc8ff97d8
nairx <nairxprv@gmail.com>
2024-12-04 20:45:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let students = [
+    { name: "Revanth", user: "revanthyellapragada17" },
+    { name: "Silpa", user: "silpa83" },
+    { name: "Ashwini", user: "ashwini2804" },
+    { name: "Madhu", user: "madhum007" },
+    { name: "Bhavisha", user: "bpatel2695" },
+    { name: "Sahiti", user: "sahiti-banda" },
+    { name: "Monali", user: "Monali40" },
+    { name: "Shankar", user: "shankar-sn" },
+    { name: "Sandhya", user: "sandhyabaddam5" },
+    { name: "Sayali", user: "sbagaitkar" },
+    { name: "Srujana", user: "srujanaaytha1" },
+    { name: "Sai Kiran", user: "kiranmothe" },
+  ];
+
+
